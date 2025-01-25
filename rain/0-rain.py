ec9dada78009709ecbe41bacc5ad88f58068624f
Nick-Lemy <n.kayiranga@alustudent.com>
2025-01-25 20:49:11 +0200

    done

--- rain/0-rain.py
+++ rain/0-rain.py
@@ -14,5 +14,13 @@ def rain(walls):
             walls.pop(0)
         elif walls[-1] == 0:
             walls.pop(-1)
-        return (len(walls) - 1)*2
+        o = 0
+        while o < len(walls):
+            print(walls)
+            if walls[o] != 0:
+                walls.pop(o)
+            o+=1
+        return len(walls)*2
+    
+print(rain([0, 1, 0, 2, 0, 3, 0, 4]))
 
