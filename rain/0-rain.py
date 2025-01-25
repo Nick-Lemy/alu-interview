0e210506481dd5c55831e65265815d433e26d98b
Nick-Lemy <n.kayiranga@alustudent.com>
2025-01-25 20:39:38 +0200

    ok

--- rain/0-rain.py
+++ rain/0-rain.py
@@ -7,4 +7,12 @@ def rain(walls):
     """
     Interestin module
     """
-    return len(walls) - 2
+    if len(walls) == 0:
+        return 0
+    else:
+        if walls[0] == 0:
+            walls.pop(0)
+        elif walls[-1] == 0:
+            walls.pop(-1)
+        return (len(walls) - 1)*2
+
